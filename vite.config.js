682100f800f40fe545a70372fbcd2502b5dc7470
praveen-alva <praveenalva83@gmail.com>
2025-09-26 23:16:42 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-});
+  plugins: [
+    tailwindcss(),
+  ],
+})
